20f2aa2a972a483e0296d9a24ee95dc0a3a6ce93
LCurcuma <zhihnadya2002@gmail.com>
2025-08-28 17:34:43 +0200

    added header component

--- web/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function Header() {
-  return (
-    <header style={{ background: "#1976d2", padding: "1rem", color: "white" }}>
-      <h1>final-project</h1>
-    </header>
-  );
-}
