ce7dbd39ba8c70e9c95db8116b006420ecf1d9e2
LCurcuma <zhihnadya2002@gmail.com>
2025-08-30 09:50:24 +0200

    edited styling of plantCard and the images of PlantCard

--- web/src/components/PlantCard/PlantCard.jsx
+++ web/src/components/PlantCard/PlantCard.jsx
@@ -5,9 +5,7 @@ export default function PlantCard({ id, name, description, price, imageUrl, aver
   return (
     <a href={`/${id}`}>
     <div className={styles.card}>
-      {/*So instead of images from database, that doesn't work, I put images of fig
-      The issue is that the website blocking importing/exporting images, that it has*/}
-      <img src="https://images.immediate.co.uk/production/volatile/sites/10/2018/02/bf8289cd-b3b2-4421-8d2b-ca8837696534-39a2ae7.jpg" className={styles.card_image}/>
+      <img src={imageUrl} className={styles.card_image}/>
       <div className={styles.card_info}>
         <h2>{name}</h2>
         <p>Average rating: {averageRating}</p>
