321967ba0098ca381b60152aaba91b68011c5a47
Elnaz-Talebi <nel.nazi1987@gmail.com>
2025-09-13 19:53:34 +0200

    Fix lint and deployment issue for web

--- web/src/app/user/login/page.js
+++ web/src/app/user/login/page.js
@@ -59,7 +59,7 @@ export default function LoginPage() {
         </button>
       </form>
       <a href="/user/register" className={styles.a}>
-        If you don't have account, so sign up
+        {"If you don't have account, so sign up"}
       </a>
     </div>
   );
